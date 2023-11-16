8e6b9a2f8dad62e41e2786512e028ef8bc7f4764
Ernbast DEV <114775576+ernbast@users.noreply.github.com>
2023-11-16 16:33:14 -0300

    Nueva version

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-document.getElementById('formulario').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var seleccionados = Array.from(document.getElementById('seleccion-multiple').selectedOptions).map(option => option.value);
-    var resultados = document.getElementById('resultados');
-    resultados.innerHTML = 'IDs seleccionados: ' + seleccionados.join(', ');
+document.addEventListener('DOMContentLoaded', () => {
+    populateSelector();
 });
+
+const data = {
+    'ID1': 'Registro asociado a ID1',
+    'ID2': 'Registro asociado a ID2',
+    // Agrega más registros según sea necesario
+};
+
+function populateSelector() {
+    const selector = document.getElementById('idSelector');
+    for (let id in data) {
+        let option = document.createElement('option');
+        option.value = id;
+        option.text = id;
+        selector.appendChild(option);
+    }
+}
+
+function searchIDs() {
+    const selectedIDs = Array.from(document.getElementById('idSelector').selectedOptions).map(opt => opt.value);
+    const results = document.getElementById('results');
+    results.innerHTML = '';
+
+    selectedIDs.forEach(id => {
+        let para = document.createElement('p');
+        para.textContent = `${id}: ${data[id]}`;
+        results.appendChild(para);
+    });
+}
