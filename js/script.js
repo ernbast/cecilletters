ca57935cf743029d4d274d2f0a254596a947263a
Ernbast DEV <114775576+ernbast@users.noreply.github.com>
2023-11-16 17:08:21 -0300

    1

--- js/script.js
+++ js/script.js
@@ -1,56 +1,41 @@
-const data = {
-    'ID1': 'Registro asociado a ID1',
-    'ID2': 'Registro asociado a ID2',
-    'ID3': 'Registro asociado a ID3',
-    // Agrega más registros según sea necesario
-};
+function searchRecords() {
+    const searchInput = document.getElementById("searchInput");
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = ""; // Limpiar resultados anteriores
 
-window.onload = function() {
-    populateSelector();
-};
+    // Obtener IDs ingresados y dividirlos por comas
+    const ids = searchInput.value.split(",");
 
-function populateSelector() {
-    const selector = document.getElementById('idSelector');
-    for (let id in data) {
-        let option = document.createElement('option');
-        option.value = id;
-        option.text = id;
-        selector.appendChild(option);
-    }
-}
+    for (const id of ids) {
+        // Simular la búsqueda de registros por ID (puedes reemplazar esto con tus propios datos)
+        const record = findRecordById(id.trim()); // Aquí deberías implementar tu lógica de búsqueda
 
-function searchIDs() {
-    let searchValue = document.getElementById('searchBox').value.toUpperCase();
-    let results = document.getElementById('results');
-    results.innerHTML = '';
-
-    for (let id in data) {
-        if (id.toUpperCase().includes(searchValue)) {
-            let para = document.createElement('p');
-            para.textContent = `${id}: ${data[id]}`;
-            results.appendChild(para);
+        if (record) {
+            // Si se encuentra el registro, mostrarlo
+            const resultItem = document.createElement("div");
+            resultItem.innerText = `ID: ${id.trim()}, Registro: ${record}`;
+            resultsDiv.appendChild(resultItem);
+        } else {
+            // Si no se encuentra el registro, mostrar un mensaje de error
+            const errorItem = document.createElement("div");
+            errorItem.innerText = `ID: ${id.trim()} no encontrado`;
+            errorItem.classList.add("error");
+            resultsDiv.appendChild(errorItem);
         }
     }
 }
 
-function searchIDs() {
-    let searchValue = document.getElementById('searchBox').value.toUpperCase();
-    let results = document.getElementById('results');
-    results.innerHTML = '';
+// Función de ejemplo para buscar registros por ID (debes implementar la tuya)
+function findRecordById(id) {
+    // Aquí puedes agregar tu lógica de búsqueda de registros
+    // Por ejemplo, podrías tener un objeto o array de registros y buscar por ID en ese objeto/array.
+    // Retornar el registro si se encuentra, o null si no se encuentra.
+    // En este ejemplo, estamos usando una función de búsqueda simulada.
+    const records = {
+        "1": "Registro 1",
+        "2": "Registro 2",
+        "3": "Registro 3",
+    };
 
-    let found = false;
-    for (let id in data) {
-        if (id.toUpperCase().includes(searchValue)) {
-            let para = document.createElement('p');
-            para.textContent = `${id}: ${data[id]}`;
-            results.appendChild(para);
-            found = true;
-        }
-    }
-
-    if (!found) {
-        results.innerHTML = '<p>No se encontraron resultados para su búsqueda.</p>';
-    }
+    return records[id] || null;
 }
-
-
