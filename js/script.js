2d89d1608fdb853ad32cee3f9954136185b085fb
Ernbast DEV <114775576+ernbast@users.noreply.github.com>
2023-11-16 18:10:00 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function searchRecords() {
         if (record) {
             // Si se encuentra el registro, mostrarlo
             const resultItem = document.createElement("div");
-            resultItem.innerText = `${id.trim()},: ${record}`;
+            resultItem.innerText = `${id.trim()}: ${record}`;
             resultsDiv.appendChild(resultItem);
         } else {
             // Si no se encuentra el registro, mostrar un mensaje de error
@@ -29,9 +29,24 @@ function searchRecords() {
 function findRecordById(id) {
     // Aquí puedes agregar tu lógica de búsqueda de registros
     const records = {
-        "1": "Registro 1",
-        "2": "Registro 2",
-        "3": "Registro 3",
+        "1": "Your sexy body has me drooling - Tu cuerpo sexy me tiene babeando",
+        "2": "I share with you unselfishly - Comparto contigo desinteresadament",
+        "3": "I acknowledge my mistakes with you - Reconozco mis errores contigo",
+        "4": "Life is a gift, and you are the most beautiful thing in it. - La vida es un regalo, y tú eres lo más lindo en él",
+        "5": "I have fantasies about what I want to do with you - Tengo fantasías acerca de lo que quiero hacer contigo",
+        "6": "I know in my heart that we'll be together again - Yo sé en mi corazón que volveremos a estar juntos de nuevo",
+        "7": "I thought I would surprise you with a quote - He pensado en sorprenderte con una cita",
+        "8": "I can't understand why I still feel a connection with you. - No puedo entender porque aún me siento en conexión contigo",
+        "9": "I ran away from you because I felt overwhelmed - Hui de ti porque me sentí abrumada",
+        "10": "I want to commit to you when I feel ready for you. - Me quiero comprometer contigo cuando me sienta lista/o para ti",
+        "11": "I need your time and commitment - Necesito tu tiempo y compromiso",
+        "12": "You need to let go so I can grow up - Necesitas soltarme para que yo pueda crecer",
+        "13": "Give me a couple of days please - Dame un par de días por favor",
+        "14": "I have acted very childish and I don't know how to approach it now. - He actuado muy infantil y no sé cómo acercarme ahora",
+        "15": "I am letting go of everything that no longer serves me - Estoy soltando todo lo que ya no me sirve",
+        "16": "I keep hoping this will work out between us - Sigo esperando que esto funcione entre nosotros",
+
+
     };
 
     return records[id] || null;
