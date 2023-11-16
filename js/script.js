606adea1070595691a97286da46fd333dcf96f37
Ernbast DEV <114775576+ernbast@users.noreply.github.com>
2023-11-16 16:48:20 -0300

    2

--- js/script.js
+++ js/script.js
@@ -1,32 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    populateSelector();
-});
+function searchIDs() {
+    let searchValue = document.getElementById('searchBox').value.toUpperCase();
+    let results = document.getElementById('results');
+    results.innerHTML = '';
 
-const data = {
-    '1': 'Registro asociado a ID1',
-    '2': 'Registro asociado a ID2',
-    '3': 'Registro asociado a ID3',
-    // Agrega más registros según sea necesario
-};
-
-function populateSelector() {
-    const selector = document.getElementById('idSelector');
+    let found = false;
     for (let id in data) {
-        let option = document.createElement('option');
-        option.value = id;
-        option.text = id;
-        selector.appendChild(option);
+        if (id.toUpperCase().includes(searchValue)) {
+            let para = document.createElement('p');
+            para.textContent = `${id}: ${data[id]}`;
+            results.appendChild(para);
+            found = true;
+        }
+    }
+
+    if (!found) {
+        results.innerHTML = '<p>No se encontraron resultados para su búsqueda.</p>';
     }
 }
 
-function searchIDs() {
-    const selectedIDs = Array.from(document.getElementById('idSelector').selectedOptions).map(opt => opt.value);
-    const results = document.getElementById('results');
-    results.innerHTML = '';
-
-    selectedIDs.forEach(id => {
-        let para = document.createElement('p');
-        para.textContent = `${id}: ${data[id]}`;
-        results.appendChild(para);
-    });
-}
